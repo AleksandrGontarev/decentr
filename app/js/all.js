764b3a0a1629e67ebc60a7fbbad66712f0a2435a
gontarev <gontarevsanya@gmail.com>
2023-05-03 20:17:26 +0300

    add modal book

--- app/js/all.js
+++ app/js/all.js
@@ -1,21 +1,28 @@
 "use strict";
 
-var buttonCards = document.querySelectorAll(".cards__btn");
-var letsAchieveBtn = document.querySelectorAll(".lets-achieve__btn");
-window.onresize = function () {
-  if (window.innerWidth <= 768) {
-    buttonCards.forEach(function (item) {
-      return item.innerText = "Join us";
-    });
-    letsAchieveBtn.forEach(function (item) {
-      return item.innerText = "Book your spot";
-    });
-  } else if (window.innerWidth > 768) {
-    buttonCards.forEach(function (item) {
-      return item.innerText = "Book a session";
-    });
-    letsAchieveBtn.forEach(function (item) {
-      return item.innerText = "Join us at Cannes";
-    });
-  }
-};
+// let buttonCards = document.querySelectorAll(".cards__btn");
+// let letsAchieveBtn = document.querySelectorAll(".lets-achieve__btn");
+// window.onresize = function () {
+//     if (window.innerWidth <= 768) {
+//         buttonCards.forEach((item) => (item.innerText = "Join us"));
+//         letsAchieveBtn.forEach((item) => (item.innerText = "Book your spot"));
+//     } else if (window.innerWidth > 768) {
+//         buttonCards.forEach((item) => (item.innerText = "Book a session"));
+//         letsAchieveBtn.forEach((item) => (item.innerText = "Join us at Cannes"));
+//     }
+// };
+var butonModalBook = document.querySelectorAll('.cards__btn');
+var modalBook = document.querySelector('.modal-book');
+var body = document.querySelector('.body');
+butonModalBook.forEach(function (item) {
+  return item.addEventListener('click', function () {
+    showModalBook();
+  });
+});
+modalBook.addEventListener('click', function () {
+  showModalBook();
+});
+function showModalBook() {
+  modalBook.classList.toggle('show');
+  body.classList.toggle('fixed');
+}
--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,24 @@
-let buttonCards = document.querySelectorAll(".cards__btn");
-let letsAchieveBtn = document.querySelectorAll(".lets-achieve__btn");
-window.onresize = function () {
-    if (window.innerWidth <= 768) {
-        buttonCards.forEach((item) => (item.innerText = "Join us"));
-        letsAchieveBtn.forEach((item) => (item.innerText = "Book your spot"));
-    } else if (window.innerWidth > 768) {
-        buttonCards.forEach((item) => (item.innerText = "Book a session"));
-        letsAchieveBtn.forEach((item) => (item.innerText = "Join us at Cannes"));
-    }
-};
+// let buttonCards = document.querySelectorAll(".cards__btn");
+// let letsAchieveBtn = document.querySelectorAll(".lets-achieve__btn");
+// window.onresize = function () {
+//     if (window.innerWidth <= 768) {
+//         buttonCards.forEach((item) => (item.innerText = "Join us"));
+//         letsAchieveBtn.forEach((item) => (item.innerText = "Book your spot"));
+//     } else if (window.innerWidth > 768) {
+//         buttonCards.forEach((item) => (item.innerText = "Book a session"));
+//         letsAchieveBtn.forEach((item) => (item.innerText = "Join us at Cannes"));
+//     }
+// };
+const butonModalBook = document.querySelectorAll('.cards__btn');
+const modalBook = document.querySelector('.modal-book');
+const body = document.querySelector('.body');
+
+butonModalBook.forEach((item) => (item.addEventListener('click', showModalBook())))
+
+modalBook.addEventListener('click', showModalBook())
+
+function showModalBook() {
+    modalBook.classList.toggle('show')
+    body.classList.toggle('fixed')
+}
 
